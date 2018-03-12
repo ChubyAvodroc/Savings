98beb67fb9ac2e2ca43ef3910bdd6a5c73edb5a1
DavidCordovaCardoza <isc.ipn.david@gmail.com>
2018-03-12 17:26:37 -0600

    [UPDATE] - Architecture Components added to the project :)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,19 +1,22 @@
 object Versions {
-    val android_gradle_plugin = "3.0.1"
-    val kotlin = "1.2.21"
+    const val android_gradle_plugin = "3.0.1"
+    const val kotlin = "1.2.21"
 
-    val minSdk = 17
-    val targetSdk = 26
-    val compileSdk = 26
-    val versionCode = 1
-    val versionName = "1.0"
+    const val minSdk = 17
+    const val targetSdk = 26
+    const val compileSdk = 26
+    const val versionCode = 1
+    const val versionName = "1.0"
 
-    val support_library = "26.1.0"
-    val constraint_layout = "1.0.2"
+    const val support_library = "26.1.0"
+    const val constraint_layout = "1.0.2"
+    const val arch_lifecycle = "1.1.0"
+    const val arch_persistence = "1.0.0"
+    const val arch_paging = "1.0.0-alpha6"
 
-    val junit = "4.12"
-    val test_runner = "1.0.1"
-    val espresso = "3.0.1"
+    const val junit = "4.12"
+    const val test_runner = "1.0.1"
+    const val espresso = "3.0.1"
 }
 
 object Deps {
@@ -28,6 +31,14 @@ object Deps {
     val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support_library}"
     val support_constraint_layout =
         "com.android.support.constraint:constraint-layout:${Versions.constraint_layout}"
+    val architecture_lifecycle = "android.arch.lifecycle:extensions:${Versions.arch_lifecycle}"
+    val architecture_lifecycle_compiler =
+        "android.arch.lifecycle:compiler:${Versions.arch_lifecycle}"
+    val architecture_persistence =
+        "android.arch.persistence.room:runtime:${Versions.arch_persistence}"
+    val architecture_persistence_compiler =
+        "android.arch.persistence.room:compiler:${Versions.arch_persistence}"
+    val architecture_paging = "android.arch.paging:runtime:${Versions.arch_paging}"
 
 }
 
@@ -40,4 +51,8 @@ object Testing {
 
     //    Espresso
     val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+
+    //    Architecture Components
+    val architecture_live_data = "android.arch.core:core-testing:${Versions.arch_lifecycle}"
+    val architecture_room = "android.arch.persistence.room:testing:${Versions.arch_persistence}"
 }
