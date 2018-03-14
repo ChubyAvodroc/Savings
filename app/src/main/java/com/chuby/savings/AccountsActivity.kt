d28a2e28e945c66172ad871f5dcecec8616fd9ed
DavidCordovaCardoza <isc.ipn.david@gmail.com>
2018-03-14 13:31:10 -0600

    [UPDATE] - MainActivity renamed to AccountsActivity

--- app/src/main/java/com/chuby/savings/AccountsActivity.kt
+++ app/src/main/java/com/chuby/savings/AccountsActivity.kt
@@ -1,12 +1,12 @@
 package com.chuby.savings
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class AccountsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_accounts)
     }
 }
