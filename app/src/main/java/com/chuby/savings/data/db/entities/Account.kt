12ec87ae4aa236e0683f2b2791323db75a4f8c75
DavidCordovaCardoza <isc.ipn.david@gmail.com>
2018-03-29 11:35:28 -0600

    [UPDATE] - ForeignKeys added to Account and Movement entities, also default value for amount on Account entity was removed

--- app/src/main/java/com/chuby/savings/data/db/entities/Account.kt
+++ app/src/main/java/com/chuby/savings/data/db/entities/Account.kt
@@ -2,14 +2,24 @@ package com.chuby.savings.data.db.entities
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
 /**
  * Created by developer on 28/03/18.
  */
-@Entity(tableName = "accounts")
+@Entity(
+    tableName = "accounts",
+    foreignKeys = [ForeignKey(
+        entity = User::class,
+        parentColumns = ["id"],
+        childColumns = ["user_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 class Account(
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
     @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "amount") var amount: Double = 0.0
+    @ColumnInfo(name = "amount") var amount: Double,
+    @ColumnInfo(name = "user_id") var userId: Int
 )
--- app/src/main/java/com/chuby/savings/data/db/entities/Movement.kt
+++ app/src/main/java/com/chuby/savings/data/db/entities/Movement.kt
@@ -2,15 +2,25 @@ package com.chuby.savings.data.db.entities
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
 
 /**
  * Created by developer on 28/03/18.
  */
-@Entity(tableName = "movements")
+@Entity(
+    tableName = "movements",
+    foreignKeys = [ForeignKey(
+        entity = Account::class,
+        parentColumns = ["id"],
+        childColumns = ["account_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 class Movement(
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
     @ColumnInfo(name = "name") var name: String,
     @ColumnInfo(name = "ammount") var ammount: Double,
-    @ColumnInfo(name = "date") var date: String
+    @ColumnInfo(name = "date") var date: String,
+    @ColumnInfo(name = "account_id") var accountId: Int
 )
