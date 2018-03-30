27eae2e864a6469b9a4ae75f3a50ad784e04d339
DavidCordovaCardoza <isc.ipn.david@gmail.com>
2018-03-30 11:22:37 -0600

    [UPDATE] - DbTest hides db instance and exposes dao objects instead, also DbTest#verifyValue method was added [UPDATE] - UserDaoTest class now uses data from DbTestUtil class

--- app/src/androidTest/java/com/chuby/savings/db/DbTest.kt
+++ app/src/androidTest/java/com/chuby/savings/db/DbTest.kt
@@ -3,6 +3,11 @@ package com.chuby.savings.db
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import com.chuby.savings.data.db.SavingsDatabase
+import com.chuby.savings.data.db.dao.AccountDao
+import com.chuby.savings.data.db.dao.MovementDao
+import com.chuby.savings.data.db.dao.UserDao
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.After
 import org.junit.Before
 
@@ -10,7 +15,16 @@ import org.junit.Before
  * Created by developer on 29/03/18.
  */
 abstract class DbTest {
-    protected lateinit var db: SavingsDatabase
+    private lateinit var db: SavingsDatabase
+    protected val accountDao: AccountDao by lazy {
+        db.accountDao()
+    }
+    protected val movementDao: MovementDao by lazy {
+        db.movementDao()
+    }
+    protected val userDao: UserDao by lazy {
+        db.userDao()
+    }
 
     @Before
     fun initDb() {
@@ -22,4 +36,8 @@ abstract class DbTest {
 
     @After
     fun closeDb() = db.close()
+
+    protected fun <Type> verifyValue(value: Type, expected: Type) {
+        MatcherAssert.assertThat(value, CoreMatchers.`is`(expected))
+    }
 }
--- app/src/androidTest/java/com/chuby/savings/db/UserDaoTest.kt
+++ app/src/androidTest/java/com/chuby/savings/db/UserDaoTest.kt
@@ -1,11 +1,8 @@
 package com.chuby.savings.db
 
 import android.support.test.runner.AndroidJUnit4
-import com.chuby.savings.data.db.entities.Address
-import com.chuby.savings.data.db.entities.User
+import com.chuby.savings.utils.DbTestUtil
 import com.chuby.savings.utils.LiveDataTestUtil
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,56 +12,30 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class UserDaoTest : DbTest() {
 
-    private val firstAddress = Address(
-        "False Street 123",
-        "False State 456",
-        "False City 789",
-        12345
-    )
-
-    private val secondAddress = Address(
-        "False Street 321",
-        "False Sate 654",
-        "False City 987",
-        54321
-    )
-
-    private val firstUSer =
-        User(
-            name = "FirstUser",
-            lastname = "Fake",
-            surname = "Mock",
-            address = firstAddress
-        )
-
-    private val secondUser = User(
-        name = "SecondUser",
-        lastname = "Mock",
-        surname = "NotReal",
-        address = secondAddress
-    )
-
     @Test
     @Throws(InterruptedException::class)
     fun emptyUsers() {
-        val users = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        val users = LiveDataTestUtil.getValue(userDao.getUsers())
+
         verifyValue(users.isEmpty(), true)
     }
 
     @Test
     fun insertUser() {
-        val rowId = db.userDao().insert(firstUSer)
+        val user = DbTestUtil.getFirstUser()
+        val rowId = userDao.insert(user)
 
         verifyValue(rowId, 1L)
 
-        val insertedUser = LiveDataTestUtil.getValue(db.userDao().getUser(1))
-        verifyValue(insertedUser.name, firstUSer.name)
+        val insertedUser = LiveDataTestUtil.getValue(userDao.getUser(1))
+
+        verifyValue(insertedUser.name, user.name)
     }
 
     @Test
     fun insertUsers() {
-        val users = listOf(firstUSer, secondUser)
-        val rowIds = db.userDao().insert(users)
+        val users = DbTestUtil.getUsers()
+        val rowIds = userDao.insert(users)
 
         verifyValue(rowIds, listOf(1L, 2L))
     }
@@ -72,20 +43,21 @@ class UserDaoTest : DbTest() {
     @Test
     @Throws(InterruptedException::class)
     fun insertUsersAndRead() {
-        val users = listOf(firstUSer, secondUser)
-        db.userDao().insert(users)
+        val users = DbTestUtil.getUsers()
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        userDao.insert(users)
+
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
 
         verifyValue(usersLoaded.size, 2)
 
-        // first user
-        val firstUser = usersLoaded[0]
+
+        val firstUser = usersLoaded[0] // first user
+        val secondUser = usersLoaded[1] // second user
+
         verifyValue(firstUser.name, "FirstUser")
         verifyValue(firstUser.address.street, "False Street 123")
 
-        // second user
-        val secondUser = usersLoaded[1]
         verifyValue(secondUser.name, "SecondUser")
         verifyValue(secondUser.address.street, "False Street 321")
     }
@@ -93,37 +65,35 @@ class UserDaoTest : DbTest() {
     @Test
     @Throws(InterruptedException::class)
     fun updateUserAndVerify() {
-        db.userDao().insert(firstUSer)
+        val user = DbTestUtil.getFirstUser()
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        userDao.insert(user)
 
-        // first user
-        val firstUser = usersLoaded[0]
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
+        val firstUser = usersLoaded[0] // first user
+
         firstUser.name = "AnotherUser"
 
-        val updatedRows = db.userDao().update(firstUser)
+        val updatedRows = userDao.update(firstUser)
+
         verifyValue(updatedRows, 1)
-
         verifyValue(firstUser.name, "AnotherUser")
-
     }
 
     @Test
     @Throws(InterruptedException::class)
     fun updateUsersAndVerify() {
-        db.userDao().insert(firstUSer)
-        db.userDao().insert(secondUser)
+        userDao.insert(DbTestUtil.getFirstUser())
+        userDao.insert(DbTestUtil.getSecondUser())
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
+        val firstUser = usersLoaded[0] // first user
+        val secondUser = usersLoaded[1] // second user
 
-        // first user
-        val firstUser = usersLoaded[0]
         firstUser.name = "AnotherUser"
-
-        val secondUser = usersLoaded[1]
         secondUser.name = "OneMoreUser"
 
-        val updatedRows = db.userDao().update(listOf(firstUser, secondUser))
+        val updatedRows = userDao.update(listOf(firstUser, secondUser))
 
         verifyValue(updatedRows, 2)
         verifyValue(firstUser.name, "AnotherUser")
@@ -134,48 +104,40 @@ class UserDaoTest : DbTest() {
     @Test
     @Throws(InterruptedException::class)
     fun deleteUserAndVerify() {
-        db.userDao().insert(listOf(firstUSer, secondUser))
+        val users = DbTestUtil.getUsers()
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        userDao.insert(users)
 
-        // first user
-        val firstUser = usersLoaded[0]
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
+        val firstUser = usersLoaded[0] // first user
+        val deletedRows = userDao.delete(firstUser)
 
-        val deletedRows = db.userDao().delete(firstUser)
         verifyValue(deletedRows, 1)
     }
 
     @Test
     @Throws(InterruptedException::class)
     fun deleteUserByIdAndVerify() {
-        db.userDao().insert(listOf(firstUSer, secondUser))
+        val users = DbTestUtil.getUsers()
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
+        userDao.insert(users)
 
-        // first user
-        val firstUser = usersLoaded[0]
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
+        val firstUser = usersLoaded[0] // first user
+        val deletedRows = userDao.delete(firstUser.id)
 
-        val deletedRows = db.userDao().delete(firstUser.id)
         verifyValue(deletedRows, 1)
-
     }
 
     @Test
     @Throws(InterruptedException::class)
     fun deleteUsersAndVerify() {
-        db.userDao().insert(firstUSer)
-        db.userDao().insert(secondUser)
+        userDao.insert(DbTestUtil.getFirstUser())
+        userDao.insert(DbTestUtil.getSecondUser())
 
-        val usersLoaded = LiveDataTestUtil.getValue(db.userDao().getUsers())
-
-        val deletedRows = db.userDao().delete(usersLoaded)
+        val usersLoaded = LiveDataTestUtil.getValue(userDao.getUsers())
+        val deletedRows = userDao.delete(usersLoaded)
 
         verifyValue(deletedRows, 2)
-
     }
-
-    private fun <Type> verifyValue(value: Type, expected: Type) {
-        assertThat(value, `is`(expected))
-    }
-
 }
